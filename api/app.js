5969caef254c1f59fe789d3a2617384e4012be4d
paulNasdaq <nasdaqpaul@gmail.com>
2022-02-12 11:16:27 +0300

    Second Commit

--- api/app.js
+++ api/app.js
@@ -1,24 +1,41 @@
 const express = require('express');
-const {collection, addDoc, getDoc, doc} = require('firebase/firestore/lite');
+const {collection, addDoc, query, where, getDocs} = require('firebase/firestore/lite');
 const bcrypt = require('bcrypt');
 
 const api = express();
 api.use(express.json());
 
 const {db} = require('../firebase.js');
-const UserCollection = collection(db, 'Users')
+const UserCollection = collection(db, 'Users');
 
 api.route('/users').post(async (req, res) => {
     //TODO: Validate user data
 
     let passwordHash = await bcrypt.hash(req.body.password, 10);
+
+    //TODO: implement validation for non-repeating email in db
     await addDoc(UserCollection, {... req.body, password: passwordHash});
     res.status(201).end();
 })
 api.route('/login').post(async (req, res) => {
     //TODO: Validate user data
 
-    const user = await getDoc(doc(db, 'user'))
+    const q = query(UserCollection, where('email', '==', req.body.email))
+    const users = await getDocs(q);
+
+    if(!users.empty){
+        const user = users.docs[1].data();
+        console.log(user);
+        if (await bcrypt.compare(req.body.password, user.password)){
+            //TODO: Generate JWT token
+            res.status(200).send('OK')
+        }
+        else {
+            res.status(400).end('Invalid email or password');
+        }
+    }else {
+        res.status(400).end('Invalid email or password');
+    }
 })
 
 api.listen(3000);
